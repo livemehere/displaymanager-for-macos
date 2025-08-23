5eeb1f1337f55df69b228b8969d7c2fa24a70100
livemehere <rhdxoals@gmail.com>
2025-08-24 05:01:56 +0900

    feat: get display lists

---
+++ src/main.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <ApplicationServices/ApplicationServices.h>
+
+using std::cout;
+using std::endl;
+
+void get_all_displays() {
+    CGDirectDisplayID ids[10];
+    uint32_t display_count;
+    CGError err = CGGetActiveDisplayList(10, ids,&display_count);
+    if (err) {
+        cout << "Error getting display list: " << err << endl;
+    } else {
+        /* success */
+        cout << "Number of active displays: " << display_count << endl;
+       for (uint32_t i = 0; i < display_count; i++) {
+           const uint32_t id = ids[i];
+           auto [origin, size] = CGDisplayBounds(id);
+
+           cout << "Display ID: " << id << endl;
+           cout << origin.x << ", " << origin.y << ", " << size.width << ", " << size.height << endl;
+       }
+    }
+}
+
+int main()
+{
+    get_all_displays();
+    return 0;
+}
